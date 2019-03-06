25cc7d4c08146fc064bec77fdb22fb6190da4561
nyan tech <nyang.tech@gmail.com>
2019-03-06 18:18:16 +0900

    add now.json

--- json-server/server.js
+++ json-server/server.js
@@ -1,17 +1,24 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-//const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+var ua = require('universal-analytics');
+
+var visitor = ua('UA-116049284-3');
+
 server.use(middlewares);
 
+// https://github.com/request/request
 var request = require('request')
 var opts = { url: 'https://raw.githubusercontent.com/kkkkkt/animal_data/master/db.json', json: true }
 
 request(opts, function (error, response, body) {
   if (error) throw error
-  const router = jsonServer.router(body)
+  const router = jsonServer.router(body);
+  console.log(Object.keys(response));
+
+  visitor.pageview("/").send(); 
 
   server.use(router)
   server.listen(port)
